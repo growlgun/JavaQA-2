3449f7ebd1c35dc2fccb547d0c0e304900260a6d
Elena Zh <vermilion7448@gmail.com>
2023-03-07 19:59:35 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 19_361;
+        int miles = 20;
+        int bonus = price / miles;
+
+        System.out.println(bonus);
+
+    }
+}
